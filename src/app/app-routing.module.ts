fafb609818fad3b24a97ed4823031f6ad8540cb2
Hulk914 <ayushthakur2009@gmail.com>
2017-06-12 22:33:41 +0530

    changed file names

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "../app/login/login.component";
-import { FirstPage } from "../app/first-page/first-page.component";
+import { HomePage } from "../app/home-page/home-page.component";
 
 const routes: Routes = [
    
-    { path: 'firstPage', component: FirstPage },
+    { path: 'firstPage', component: HomePage },
     { path: 'login', component: LoginComponent},
     { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from "./login/login.component";
-import { FirstPage } from "./first-page/first-page.component";
+import { HomePage } from "./home-page/home-page.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpModule } from "@angular/http";
 import { GetData } from "./services/get-data.component";
 
 @NgModule({
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpModule],
-  declarations: [AppComponent, LoginComponent, FirstPage],
+  declarations: [AppComponent, LoginComponent, HomePage],
   bootstrap: [AppComponent],
   providers: [GetData]
 })
